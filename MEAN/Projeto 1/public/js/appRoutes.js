7e4ba16656ce6ccab8f3a0e33880766c276dae97
Vinicius <ambrosi.vinicius@gmail.com>
2016-07-21 14:47:04 -0300

    fix(appRoutes): corrigir modules para module.

--- MEAN/Projeto 1/public/js/appRoutes.js
+++ MEAN/Projeto 1/public/js/appRoutes.js
@@ -1,15 +1,16 @@
-angular.modules('appRoutes', [])
-    .config(['$routeProvider', '$locationProvider',
-         function($routeProvider, $locationProvider){
-            $routeProvider
-                .when('/', {
-                    templateUrl: 'views/home.html',
-                    controller: 'MainController'
-                })
-                .when('/nerds', {
-                    templateUrl: 'views/nerd.html',
-                    controller: 'NerdController'
-                });
-            $locationProvider.html5Mode(true);
-        }
-    ]);
+angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: '/views/home.html',
+			controller: 'MainController'
+		})
+
+		.when('/nerds', {
+			templateUrl: '/views/nerd.html',
+			controller: 'NerdController'
+		})
+	$locationProvider.html5Mode({ //remover # das urls
+		enabled: true
+	});
+
+}]);
